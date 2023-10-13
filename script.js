b15172efb5a160d23984a01c60ab59620e482c7c
kath21623 <147115422+kath21623@users.noreply.github.com>
2023-10-13 13:29:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const acceptButton = document.getElementById("acceptButton");
 
     acceptButton.addEventListener("click", function() {
-        alert("Thank you for saying 'Yes'! You mean the world to me. Redirecting...");
+        alert("Thank you for saying 'Yes'! Iloveyou babyy Always. Redirecting...");
         window.location.href = "https://m.me/mickie.carcasona";
     });
 });
